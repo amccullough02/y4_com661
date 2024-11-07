d7f56ab223c25e3ec4978124a3e3b13a0398a09b
Alex McCullough <alexmccullough@outlook.com>
2024-11-07 13:39:12 +0000

    FE12 complete

--- BizFE/src/app/businesses.component.ts
+++ BizFE/src/app/businesses.component.ts
@@ -1,29 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Data, RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'businesses',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [DataService],
   templateUrl: './businesses.component.html',
   styleUrl: './businesses.component.css',
 })
 export class BusinessesComponent {
-  business_list = [
-    {
-      name: 'Pizza Mountain',
-      town: 'Coleraine',
-      rating: 5,
-    },
-    {
-      name: 'Chilli Champions',
-      town: 'Portadown',
-      rating: 4,
-    },
-    {
-      name: 'Sausage Supreme',
-      town: 'Derry',
-      rating: 3,
-    },
-  ];
+  business_list: any;
+  page: number = 1;
+
+  constructor(public dataService: DataService) {}
+
+  ngOnInit() {
+    if (sessionStorage['page']) {
+      this.page = Number(sessionStorage['page']);
+    }
+    this.business_list = this.dataService.getBusinesses(this.page);
+  }
+
+  previousPage() {
+    if (this.page > 1) {
+      this.page = this.page - 1;
+      sessionStorage['page'] = this.page;
+      this.business_list = this.dataService.getBusinesses(this.page);
+    }
+  }
+  nextPage() {
+    if (this.page < this.dataService.getLastPageNumber()) {
+      this.page = this.page + 1;
+      sessionStorage['page'] = this.page;
+      this.business_list = this.dataService.getBusinesses(this.page);
+    }
+  }
 }
