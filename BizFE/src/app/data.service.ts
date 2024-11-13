290c895b3c2966a36d4e6aa37ff6adcb144057a7
Alex McCullough <alexmccullough@outlook.com>
2024-11-13 21:32:35 +0000

    feat: FE13 completed

--- BizFE/src/app/data.service.ts
+++ BizFE/src/app/data.service.ts
@@ -3,6 +3,16 @@ import jsonData from '../assets/businesses.json';
 export class DataService {
   pageSize: number = 3;
 
+  getBusiness(id: any) {
+    let dataToReturn: any[] = [];
+    jsonData.forEach( function(business) {
+      if (business['_id'] == id) {
+        dataToReturn.push(business);
+      }
+    })
+    return dataToReturn;
+  }
+
   getBusinesses(page: number) {
     let pageStart = (page - 1) * this.pageSize;
     let pageEnd = pageStart + this.pageSize;
