94e874ff99cc8cd9c1521754eb2f96e81a67d4e9
Alex McCullough <alexmccullough@outlook.com>
2024-11-14 13:39:22 +0000

    FE14 complete

--- BizFE/src/app/business.component.ts
+++ BizFE/src/app/business.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, ActivatedRoute } from '@angular/router';
 import { DataService } from './data.service';
+import { CommonModule } from '@angular/common';
+import { GoogleMapsModule } from '@angular/google-maps';
+
 @Component({
   selector: 'business',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, GoogleMapsModule],
   providers: [DataService],
   templateUrl: './business.component.html',
   styleUrl: './business.component.css',
 })
 export class BusinessComponent {
   business_list: any;
+  business_lat: any;
+  business_lng: any;
+  map_options: google.maps.MapOptions = {};
+  map_locations: any[] = [];
+  loremIpsum: any;
+  temperature: any;
+  temperatureColour: any;
+  weather: any;
+  weatherIcon: any;
+  weatherIconURL: any;
 
   constructor(public dataService: DataService, private route: ActivatedRoute) {}
 
@@ -18,5 +31,30 @@ export class BusinessComponent {
     this.business_list = this.dataService.getBusiness(
       this.route.snapshot.paramMap.get('id')
     );
+    this.business_lat = this.business_list[0].latitude;
+    this.business_lng = this.business_list[0].longitude;
+    this.map_options = {
+      mapId: 'DEMO_MAP_ID',
+      center: { lat: this.business_lat, lng: this.business_lng },
+      zoom: 13,
+    };
+    this.map_locations.push({ lat: this.business_lat, lng: this.business_lng });
+    this.dataService.getLoremIpsum(1).subscribe((response: any) => {
+      this.loremIpsum = response.text.slice(0, 400);
+    });
+    this.dataService
+      .getCurrentWeather(this.business_lat, this.business_lng)
+      .subscribe((response: any) => {
+        let weatherResponse = response['weather'][0]['description'];
+        this.temperature = Math.round(response['main']['temp']);
+        this.weather =
+          weatherResponse[0].toUpperCase() + weatherResponse.slice(1);
+        this.weatherIcon = response['weather'][0]['icon'];
+        this.weatherIconURL =
+          'https://openweathermap.org/img/wn/' + this.weatherIcon + '@4x.png';
+        this.temperatureColour = this.dataService.getTemperatureColour(
+          this.temperature
+        );
+      });
   }
 }
--- BizFE/src/app/data.service.ts
+++ BizFE/src/app/data.service.ts
@@ -1,15 +1,23 @@
 import jsonData from '../assets/businesses.json';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class DataService {
   pageSize: number = 3;
 
+  constructor(private http: HttpClient) {}
+
   getBusiness(id: any) {
     let dataToReturn: any[] = [];
-    jsonData.forEach( function(business) {
+    jsonData.forEach(function (business) {
       if (business['_id'] == id) {
         dataToReturn.push(business);
       }
-    })
+    });
     return dataToReturn;
   }
 
@@ -20,6 +28,35 @@ export class DataService {
   }
 
   getLastPageNumber() {
-    return Math.ceil(jsonData.length / this.pageSize)
+    return Math.ceil(jsonData.length / this.pageSize);
+  }
+
+  getLoremIpsum(paragraphs: number): Observable<any> {
+    let API_key = '6EMn+PBqmAduX1JbGS376A==7RbIGOmtwEne4jgF';
+    return this.http.get<any>(
+      'https://api.api-ninjas.com/v1/' + 'loremipsum?paragraphs=' + paragraphs,
+      { headers: { 'X-Api-Key': API_key } }
+    );
+  }
+
+  getCurrentWeather(lat: number, lon: number) {
+    let API_key = '46958caf9d9e05bf86f3d63983a06b49';
+    return this.http.get<any>(
+      'https://api.openweathermap.org/data/2.5/' +
+        'weather?lat=' +
+        lat +
+        '&lon=' +
+        lon +
+        '&units=metric&appid=' +
+        API_key
+    );
+  }
+
+  getTemperatureColour(temp: number) {
+    if (temp <= 5) return '#0000ff';
+    else if (temp <= 12) return '#00ff00';
+    else if (temp <= 17) return '#ffff00';
+    else if (temp <= 25) return '#ff7f00';
+    else return '#ff0000';
   }
 }
